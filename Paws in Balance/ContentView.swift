97b09bfcbab5f07cf9ad153b3a48470ff87f56a5
Cody Van Dyke <cvd97@ou.edu>
2023-10-18 14:42:54 -0500

    added UI fraamework that I can build off of

--- Paws in Balance/ContentView.swift
+++ Paws in Balance/ContentView.swift
@@ -1,88 +1,60 @@
-//
-//  ContentView.swift
-//  Paws in Balance
-//
-//  Created by Cody Van Dyke on 10/18/23.
-//
-
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
+    // 1. Properties (State, Binding, ObservedObject, etc.)
+    @State private var isButtonPressed = false
 
     var body: some View {
+        // 2. Main layout
         NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
+            VStack {
+                // 3. Subviews or components
+                headerView
+                mainContentView
+                footerView
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+            .navigationTitle("SwiftUI App")
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
+extension ContentView {
+    // 4. Individual component views
+
+    var headerView: some View {
+        Text("Header")
+            .font(.largeTitle)
+            .padding()
+    }
+
+    var mainContentView: some View {
+        VStack {
+            Text("Main Content")
+                .font(.title)
+                .padding()
+
+            Button(action: {
+                isButtonPressed.toggle()
+            }) {
+                Text(isButtonPressed ? "Button Pressed" : "Press Me")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+        }
+    }
+
+    var footerView: some View {
+        Text("Footer")
+            .font(.caption)
+            .padding()
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
+
