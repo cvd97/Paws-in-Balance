f4f165c1e21bf9d80754804e00d7d856da000203
Cody Van Dyke <cvd97@ou.edu>
2023-10-18 14:19:34 -0500

    Initial Commit

---
+++ Paws in Balance/Paws_in_BalanceApp.swift
@@ -0,0 +1,20 @@
+//
+//  Paws_in_BalanceApp.swift
+//  Paws in Balance
+//
+//  Created by Cody Van Dyke on 10/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Paws_in_BalanceApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ Paws in BalanceUITests/Paws_in_BalanceUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Paws_in_BalanceUITestsLaunchTests.swift
+//  Paws in BalanceUITests
+//
+//  Created by Cody Van Dyke on 10/18/23.
+//
+
+import XCTest
+
+final class Paws_in_BalanceUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
